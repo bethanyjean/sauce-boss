4535de3552230de686ee25898c23b87bd9d77b84
Harrison Daniel <hd4n@outlook.com>
2022-07-28 19:36:12 -0400

    added gitignores and start to frontend structure

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -42,6 +42,7 @@ const resolvers = {
   },
   login: async (parent, { email, password }) => {
     const user = await User.findOne({ email });
+    console.log(user)
 
     if (!user) {
       throw new AuthenticationError('Incorrect credentials');
