8b775216af4ccedfcacd7492fa28640fc01919ef
Bethany Griffin <99105089+bethanyjean@users.noreply.github.com>
2022-07-28 20:08:16 -0400

    Updated the database name
    Database name didn't get included in the git add.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sauce-boss',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sauceDb',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
