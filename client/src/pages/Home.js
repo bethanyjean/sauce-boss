ab4a174c44eef82d30196364cff9724568186d16
Bethany Griffin <bethanyjeanbg@gmail.com>
2022-07-30 13:45:00 -0400

    added colors

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import { sauces } from "../data";
 
 export default function Home() {
   return (
-    <section id="sauces" className="text-gray-400 bg-gray-900 body-font">
+    <section id="sauces" className="text-red bg-beige body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
